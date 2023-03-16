7943bcaee451bc4259963f122cd2ce4cba8d4007
Linh Nguyen <linh.nguyenkhanhib@gmail.com>
2023-03-16 15:41:36 +0200

    make small controllers for each route and refactor codes

--- backend/journeyRoute.js
+++ backend/journeyRoute.js
@@ -1,16 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const Trips = require("./model/tripsModel");
+const {
+  getAllJourney,
+  getOneJourney,
+} = require("../backend/controllers/Journey");
 
-// get one trip
-router.get("/:id", (req, res) => {
-  // req.params.id;
-});
-// get all trips
-router.get("/", async (req, res) => {
-  const posts = await Trips.find();
-  res.send(posts);
-  res.status(200).json({ message: "get journey" });
-});
+router.route("/").get(getAllJourney);
+
+router.route("/:id").get(getOneJourney);
 
 module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -5,15 +5,17 @@ const cors = require("cors");
 const colors = require("colors");
 const connectDB = require("./config/db");
 
+const journeyRoute = require("./journeyRoute");
+
 connectDB();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use("/:id", require("./journeyRoute"))
+app.use("/api/journey/:id", journeyRoute);
 
-app.use("/api/journey", require("./journeyRoute"));
+app.use("/api/journey", journeyRoute);
 
 app.listen(PORT, () => {
   console.log(`Server starting on port ${PORT}`);
