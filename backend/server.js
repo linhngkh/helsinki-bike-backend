7a045e29b2b157b2af843607e93da139193acf14
Linh Nguyen <linh.nguyenkhanhib@gmail.com>
2023-03-16 09:38:42 +0200

    connected with mongodb

--- backend/server.js
+++ backend/server.js
@@ -2,13 +2,15 @@ const express = require("express");
 const dotenv = require("dotenv").config();
 const PORT = process.env.PORT || 8000;
 const cors = require("cors");
+const colors = require("colors");
+const connectDB = require("./config/db");
+
+connectDB();
 
 const app = express();
 app.use(cors());
 
-app.get("/api/journey", (req, res) => {
-  res.json({ message: "get journey" });
-});
+app.use("/api/journey", require("./journeyRoute"));
 
 app.listen(PORT, () => {
   console.log(`Server starting on port ${PORT}`);
