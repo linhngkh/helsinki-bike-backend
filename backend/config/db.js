a83d6dbedaf52e526299aff167bce120f7cc1b55
Linh Nguyen <linh.nguyenkhanhib@gmail.com>
2023-04-14 08:48:47 +0300

    update db link

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,7 @@
 const mongoose = require("mongoose");
 
+// MONGO_URI=mongodb+srv://linhng:linhng123@cluster0.ubkr5bf.mongodb.net/citybikes
+
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI, {
