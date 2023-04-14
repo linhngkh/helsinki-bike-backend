a72d0b03c34bdb135f3124711f6b095a9a4d57ee
Linh Nguyen <linh.nguyenkhanhib@gmail.com>
2023-04-14 08:47:01 +0300

    update readme

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,11 +1,8 @@
 const mongoose = require("mongoose");
 
-const DB =
-  "mongodb+srv://linhng:linhng123@cluster0.ubkr5bf.mongodb.net/citybikes";
-
 const connectDB = async () => {
   try {
-    await mongoose.connect(DB, {
+    await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
